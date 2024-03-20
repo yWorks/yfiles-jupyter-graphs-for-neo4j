5e69fd778f2dbd54738cb67c259326d7a1d84dbd
Gesa <schoenbohm.gesa@t-online.de>
2024-03-20 15:49:01 +0100

    added nodebindings configurations

--- src/yfiles_jupyter_graphs_for_neo4j/Yfiles_Neo4j_Graphs.py
+++ src/yfiles_jupyter_graphs_for_neo4j/Yfiles_Neo4j_Graphs.py
@@ -8,6 +8,8 @@ from typing import Optional, Any
 
 class YfilesNeo4jGraphs:
     _driver = None
+    _node_configurations = {}
+    _edge_configurations = {}
 
     def __init__(self, driver: Optional[Any] = None):
         if driver is not None:
@@ -32,14 +34,37 @@ class YfilesNeo4jGraphs:
         """
         return self._driver
 
-    def show_cypher(self, cypher):
+    def show_cypher(self, cypher, **kwargs):
         """
         main function
         :param cypher: str, Send a data query to the neo4j database
+               **kwargs: variable declarations usable in cypher
         """
         if self._driver is not None:
-            widget = GraphWidget(graph=self._session.run(cypher).graph())
+            widget = GraphWidget(graph=self._session.run(cypher, **kwargs).graph())
+            widget.set_node_styles_mapping(lambda node: self._node_configurations.get(node["properties"]["label"], {}))
+            widget.set_node_label_mapping(lambda node: node["properties"][
+                self._node_configurations.get(node["properties"]["label"], {"label": "label"})["label"]])
+
             widget.show()
         else:
             raise Exception("no driver specified")
 
+
+    #TODO add more bindings
+    def add_node_configuration(self, type, textbinding=None, color=None):
+        if textbinding is not None and color is not None:
+            self._node_configurations[type] = {'label': textbinding, 'color': color}
+        elif textbinding is None and color is not None:
+            self._node_configurations[type] = {'color': color}
+        elif textbinding is not None and color is None:
+            self._node_configurations[type] = {'label': textbinding}
+
+
+    def add_relation_configuration(self, type, textbinding=None, color=None):
+        if textbinding is not None and color is not None:
+            self._edge_configurations[type] = {'label': textbinding, 'color': color}
+        elif textbinding is None and color is not None:
+            self._edge_configurations[type] = {'color': color}
+        elif textbinding is not None and color is None:
+            self._edge_configurations[type] = {'label': textbinding}
