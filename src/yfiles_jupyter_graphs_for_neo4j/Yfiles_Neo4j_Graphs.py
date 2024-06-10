124dece68c4beb8f7198b150895189235c4909df
Fabian Schwarzkopf <fabian.schwarzkopf@yworks.com>
2024-06-10 09:02:40 +0200

    Improve default mapping for item texts, #2

--- src/yfiles_jupyter_graphs_for_neo4j/Yfiles_Neo4j_Graphs.py
+++ src/yfiles_jupyter_graphs_for_neo4j/Yfiles_Neo4j_Graphs.py
@@ -3,13 +3,15 @@
 The main Neo4jGraphWidget class is defined in this module.
 
 """
+from typing import Dict
+
 from yfiles_jupyter_graphs import GraphWidget
 
 # TODO maybe change to get dynamically when adding bindings
 
 POSSIBLE_NODE_BINDINGS = {'color', 'size', 'type', 'styles', 'scale_factor', 'position', 'layout', 'property', 'label'}
 POSSIBLE_EDGE_BINDINGS = {'color', 'thickness_factor', 'property', 'label'}
-
+NEO4J_LABEL_KEYS = ['name', 'title', 'label', 'description', 'caption', 'text']
 
 class Neo4jGraphWidget:
     _driver = None
@@ -99,64 +101,59 @@ class Neo4jGraphWidget:
             widget = GraphWidget(overview_enabled=self._overview, context_start_with=self._context_start_with,
                                  widget_layout=self._layout, license=self._license,
                                  graph=self._session.run(cypher, **kwargs).graph())
-            self.apply_node_mappings(widget)
-            self.apply_edge_mappings(widget)
+            self.__apply_node_mappings(widget)
+            self.__apply_edge_mappings(widget)
 
             self._widget = widget
             widget.show()
         else:
             raise Exception("no driver specified")
 
-    def apply_node_mappings(self, widget):
+    @staticmethod
+    def __get_neo4j_item_text(element: Dict):
+        lowercase_element_props = {key.lower(): value for key, value in element.get('properties', {}).items()}
+        for key in NEO4J_LABEL_KEYS:
+            if key in lowercase_element_props:
+                return str(lowercase_element_props[key])
+        return None
 
+    @staticmethod
+    def __configuration_mapper_factory(binding_key, configurations, default_mapping):
+        def mapping(index, item: Dict):
+            label = item["properties"]["label"]  # yjg stores the neo4j node/relationship type in properties["label"]
+            if label in configurations and binding_key in configurations.get(label):
+                # mapping
+                if callable(configurations.get(label)[binding_key]):
+                    return configurations.get(label)[binding_key](item)
+                # property name
+                elif configurations.get(label)[binding_key] in item["properties"]:
+                    return item["properties"][configurations.get(label).get(binding_key)]
+                # constant value
+                else:
+                    return configurations.get(label).get(binding_key)
+
+            if binding_key == "label":
+                return Neo4jGraphWidget.__get_neo4j_item_text(item)
+            else:
+                return default_mapping(index, item)
+        return mapping
+
+    def __apply_node_mappings(self, widget):
         for key in POSSIBLE_NODE_BINDINGS:
-            def wrapper(current_key):
-                def mapping(index, node):
-                    label = node["properties"]["label"]
-                    if label in self._node_configurations and current_key in self._node_configurations.get(label):
-                        # mapping
-                        if callable(self._node_configurations.get(label)[current_key]):
-                            return self._node_configurations.get(label)[current_key](node)
-                        # property name
-                        elif self._node_configurations.get(label)[current_key] in node["properties"]:
-                            return node["properties"][self._node_configurations.get(label).get(current_key)]
-                        # constant value
-                        else:
-                            return self._node_configurations.get(label).get(current_key)
-                    default = getattr(widget, f"default_node_{current_key}_mapping")
-                    return default(index, node)
+            default_mapping = getattr(widget, f"default_node_{key}_mapping")
+            setattr(widget, f"_node_{key}_mapping",
+                    Neo4jGraphWidget.__configuration_mapper_factory(key, self._node_configurations, default_mapping))
 
-                return mapping
-
-            setattr(widget, f"_node_{key}_mapping", wrapper(key))
-
-    def apply_edge_mappings(self, widget):
-
+    def __apply_edge_mappings(self, widget):
         for key in POSSIBLE_EDGE_BINDINGS:
-            def wrapper(current_key):
-                def mapping(index, edge):
-                    label = edge["properties"]["label"]
-                    if label in self._edge_configurations and current_key in self._edge_configurations.get(label):
-                        # mapping
-                        if callable(self._edge_configurations.get(label)[current_key]):
-                            return self._edge_configurations.get(label)[current_key](edge)
-                        # property name
-                        elif self._edge_configurations.get(label)[current_key] in edge["properties"]:
-                            return edge["properties"][self._edge_configurations.get(label).get(current_key)]
-                        # constant value
-                        else:
-                            return self._edge_configurations.get(label).get(current_key)
-                    default = getattr(widget, f"default_edge_{current_key}_mapping")
-                    return default(index, edge)
-
-                return mapping
-
-            setattr(widget, f"_edge_{key}_mapping", wrapper(key))
+            default_mapping = getattr(widget, f"default_edge_{key}_mapping")
+            setattr(widget, f"_edge_{key}_mapping",
+                    Neo4jGraphWidget.__configuration_mapper_factory(key, self._edge_configurations, default_mapping))
 
     def add_node_configuration(self, label, **kwargs):
         # this wrapper uses "text" as text binding in the graph
         # in contrast to "label" which is used in yfiles-jupyter-graphs
-        text_binding = kwargs.pop("text", 'label')
+        text_binding = kwargs.pop("text", None)
         config = kwargs
         if text_binding is not None:
             config["label"] = text_binding
@@ -165,7 +162,7 @@ class Neo4jGraphWidget:
     def add_relationship_configuration(self, type, **kwargs):
         # this wrapper uses "text" as text binding in the graph
         # in contrast to "label" which is used in yfiles-jupyter-graphs
-        text_binding = kwargs.pop("text", 'label')
+        text_binding = kwargs.pop("text", None)
         config = kwargs
         if text_binding is not None:
             config["label"] = text_binding
